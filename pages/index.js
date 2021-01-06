0c09c5e8fef948fb080c2f7889edb93f5d1fc5cc
jasontcrabtree <jasontcrabtree@gmail.com>
2021-01-06 23:22:19 +1300

    added firebase and blog posts to homepage broken TODO fix googly eyes

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,56 @@
+/* eslint-disable react/no-danger */
 // This component represents the index page for the site. You
 // can read more about Pages in the Next.js docs at:
 // https://nextjs.org/docs/basic-features/pages
 
+import { getPosts } from '@lib/firebase';
 import styles from '@styles/index.module.scss';
 
-const HomePage = () => (
+// Date formating function
+const getFormattedDate = (milliseconds) => {
+  const formatOptions = {
+    weekday: 'long',
+    month: 'long',
+    day: 'numeric',
+    year: 'numeric',
+    timeZone: 'UTC',
+  };
+  const date = new Date(milliseconds);
+  return date.toLocaleDateString(undefined, formatOptions);
+};
+
+const HomePage = ({ posts }) => (
   <div className={styles.HomePage}>
-    <h1>Hello from the Home Page!</h1>
+    <h1>Blog Posts</h1>
+    {posts.map((post) => (
+      <article>
+        <img src="" alt="" />
+        <div>
+          <h2>{post.title}</h2>
+          <span>{getFormattedDate(post.dateCreated)}</span>
+          <p
+            dangerouslySetInnerHTML={{
+              __html: `${post.content.substring(0, 200)}...`,
+            }}
+          />
+        </div>
+      </article>
+    ))}
   </div>
 );
+
+/* This is for fetching data every time the page is visited. We do this
+so that we don't have to redploy the site every time we add a blog post.
+You can read more about this in the Next.js docs at:
+https://nextjs.org/docs/basic-features/data-fetching#getserversideprops-server-side-rendering */
+export async function getServerSideProps() {
+  const posts = await getPosts();
+
+  return {
+    props: {
+      posts,
+    },
+  };
+}
 
 export default HomePage;
